771a7bc023c13a3461cb51455394414ffcb878b8
JakubJosefKrasa <jjkrasa@gmail.com>
2024-11-10 00:43:03 +0100

    Updated useState initial value to fix bug where protected pages weren't loaded when entered in url for first time

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,22 +1,32 @@
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren, useState } from "react";
 
 import { AuthContextType, AuthType } from "@/types/types";
 import { AuthContext } from "@/context/authContext";
 
 export default function AuthProvider({ children }: PropsWithChildren) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [email, setEmail] = useState<string | null>(null);
-
-  useEffect(() => {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
     const authObjStr = localStorage.getItem("auth");
 
     if (authObjStr) {
       const storedAuth = JSON.parse(authObjStr) as AuthContextType;
 
-      setIsLoggedIn(storedAuth.isLoggedIn);
-      setEmail(storedAuth.email);
+      return storedAuth.isLoggedIn;
     }
-  }, []);
+
+    return false;
+  });
+
+  const [email, setEmail] = useState<string | null>(() => {
+    const authObjStr = localStorage.getItem("auth");
+
+    if (authObjStr) {
+      const storedAuth = JSON.parse(authObjStr) as AuthContextType;
+
+      return storedAuth.email;
+    }
+
+    return null;
+  });
 
   function setAuth(auth: AuthType) {
     const { isLoggedIn, email } = auth;
