19920b46506f2a27b22ce744dae6e123a26ce86f
JakubJosefKrasa <jjkrasa@gmail.com>
2025-03-10 07:22:11 +0100

    regex for password changed

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -17,34 +17,38 @@ export const loginSchema = z
 
 export type LoginType = z.infer<typeof loginSchema>;
 
+export const emailValidator = z
+  .string({
+    required_error: "Required",
+    invalid_type_error: "Must be string",
+  })
+  .trim()
+  .min(1, "Emailová adresa nesmí být prázdná")
+  .toLowerCase()
+  .regex(
+    new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}"),
+    "Neplatný email!"
+  );
+
+export const passwordValidator = z
+  .string()
+  .trim()
+  .min(1, "Heslo nesmí být prázdné")
+  .regex(
+    new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{7,255}$"),
+    "Heslo musí obsahovat alespoň jedno velké písmeno, číslici, speciální znak a být dlouhé minimálně 7 znaků!"
+  );
+
+export const confirmPasswordValidator = z
+  .string()
+  .trim()
+  .min(1, "Potvrzovací heslo nesmí být prázdné");
+
 export const registerSchema = z
   .object({
-    email: z
-      .string({
-        required_error: "Required",
-        invalid_type_error: "Must be string",
-      })
-      .trim()
-      .min(1, "Emailová adresa nesmí být prázdná")
-      .toLowerCase()
-      .regex(
-        new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}"),
-        "Neplatný email!"
-      ),
-    password: z
-      .string()
-      .trim()
-      .min(1, "Heslo nesmí být prázdné")
-      .regex(
-        new RegExp(
-          "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{7,255}$"
-        ),
-        "Heslo musí obsahovat alespoň jedno velké písmeno, číslici, speciální znak a být dlouhé minimálně 7 znaků!"
-      ),
-    confirmPassword: z
-      .string()
-      .trim()
-      .min(1, "Potvrzovací heslo nesmí být prázdné"),
+    email: emailValidator,
+    password: passwordValidator,
+    confirmPassword: confirmPasswordValidator,
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Hesla se musí shodovat",
